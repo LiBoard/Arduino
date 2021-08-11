8de4ecef291a11a3b21f7b2b8e17f346bbfd9428
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2021-08-11 15:10:15 +0200

    Update Copyright

--- examples/serial_binboard/serial_binboard.ino
+++ examples/serial_binboard/serial_binboard.ino
@@ -1,3 +1,18 @@
+/* LiBoard
+ * Copyright (C) 2021 Philipp Leclercq
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
 #include "LiBoard.h"
 
 const unsigned short THRESHOLD = 100;
